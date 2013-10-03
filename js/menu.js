e099cfc4f005a36e9b4b7a54fad11e868d640b01
kikisaeba <kikisaeba@gmail.com>
2013-10-03 18:23:39 +0200

    Make urls bookmarkable

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 function get_menu_definition() {
 	return [
 	{ title: "Documentation", children: [
-	 	{ title: "Contributing", closed: true, url: "documentation/contributing/index.html", children: [
+	 	{ code: "contrib", title: "Contributing", closed: true, url: "documentation/contributing/index.html", children: [
 		 	{ title: "Hack", url: "documentation/contributing/code.html" },
-			{ title: "Document", closed: true, children: [
+			{ code: "docs", title: "Document", closed: true, children: [
 				{ title: "Setting up Jekyll on Windows", url: "documentation/contributing/windows.html" },
 				{ title: "Setting up Jekyll on Linux", url: "documentation/contributing/linux.html" }
 			]},
 		]},
-		{ title: "Installing", children: [
+		{ code: "install", title: "Installing", closed: true, children: [
 			{ title: "Windows", url: "documentation/installing/windows.html" },
 			{ title: "MacOSX", url: "documentation/installing/macosx.html" }
 		]},
